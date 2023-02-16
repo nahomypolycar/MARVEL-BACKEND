8a6a8e2245916d1d9359563e43bfbde56ef6da9c
nahomypolycar <112959201+nahomypolycar@users.noreply.github.com>
2023-02-16 19:49:37 +0100

    envoi du code dans la route charactersby Id

--- index.js
+++ index.js
@@ -23,14 +23,6 @@ app.get("/characters", async (req, res) => {
       "https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=" +
         process.env.MARVEL_API_KEY
     );
-
-    const comicsTab = response.data.results;
-
-    console.log("comicsTab >>>", comicsTab); // ne s'affiche pas
-
-    const charactersId = comicsTab.map((element, index) => {
-      return res.status(200).json({ element: element });
-    });
     console.log("response >>>", response.data);
     return res.status(200).json({ response: response.data });
   } catch (error) {
@@ -55,7 +47,18 @@ app.get("/comics/:characterId", (req, res) => {
     const response = axios.get(
       "https://lereacteur-marvel-api.herokuapp.com/comics/5fc8ba1fdc33470f788f88b3?apiKey=YOUR_API_KEY"
     );
-  } catch (error) {}
+    const comicsTab = response.data.results;
+
+    console.log("comicsTab >>>", comicsTab); // ne s'affiche pas
+
+    const charactersId = comicsTab.map((element, index) => {
+      return res.status(200).json({ element: element });
+    });
+    console.log("response >>>", response.data);
+    return res.status(200).json({ response: response.data });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 });
 
 app.listen(3000, () => {
